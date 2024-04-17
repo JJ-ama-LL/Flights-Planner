e54ac9d1b46eaa2ee4a932bf89e3f960e03a18c9
Jamal Mohammed <157765656+JJ-ama-LL@users.noreply.github.com>
2024-04-17 05:02:51 +0000

    firebaseConfig setup 	new file:   public/firebaseConfig.js

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRrHKa-KwC-OsddM9uVt3OmssHsEK80ag",
+    authDomain: "info-1601---travel-hub.firebaseapp.com",
+    projectId: "info-1601---travel-hub",
+    storageBucket: "info-1601---travel-hub.appspot.com",
+    messagingSenderId: "76582549194",
+    appId: "1:76582549194:web:080ab5215a649847f72633",
+    measurementId: "G-Z6GYVWZBH2"
+  };
+
+  export default firebaseConfig
