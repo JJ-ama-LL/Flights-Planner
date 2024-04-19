e120ebc06029a301cf0e4ba5b65a693e176960e7
Jamal Mohammed <157765656+JJ-ama-LL@users.noreply.github.com>
2024-04-19 02:19:36 +0000

    starting of auth

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -4,8 +4,8 @@ const firebaseConfig = {
   projectId: "info-1601---travel-hub-7b021",
   storageBucket: "info-1601---travel-hub-7b021.appspot.com",
   messagingSenderId: "1010222563636",
-  appId: "1:1010222563636:web:a78c861fc8b31339736561",
-  measurementId: "G-4CP0DY2Z49"
+  appId: "1:1010222563636:web:8c45de09070d4107736561",
+  measurementId: "G-GQDWCFRLMJ"
 };
 
-  export default firebaseConfig
+export default firebaseConfig
